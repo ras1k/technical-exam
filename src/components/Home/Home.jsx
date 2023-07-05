90b603a3641ba7b01614792456395e1d3e9cc500
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-06 00:48:11 +0600

    fixed responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,15 +8,15 @@ function Home() {
     fetch('https://technical-exam-server.vercel.app/services')
       .then(res => res.json())
       .then(data => setServices(data))
-  }, [])
+  }, []);
 
   return (
-    <div>
+    <div className=''>
       <div className='text-center'>
         <h1 className='text-4xl font-bold md:mt-6'>Explore Our Storage Space, Co-Working Space, <br /> Co-Warehouse & Services</h1>
         <p className='text-slate-600 mt-4 md:mt-10'>Carefully crafted for a hassle-free and affordable experience</p>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-4 mt-6 gap-y-24 gap-x-6'>
+      <div className='grid grid-cols-1 md:grid-cols-4 mt-6 gap-y-24 md:gap-x-6'>
         {
           services.map(service => <Service
             key={service.id}
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -4,13 +4,19 @@ import { FaArrowRight } from 'react-icons/fa';
 const Service = ({ service }) => {
     return (
         <div className=''>
-            <img src={service.logo} className='w-full lg:w-4/6 h-full' alt="" />
-            <div className='flex flex-col space-y-4'>
-                <div className='md:h-[120px]'>
+            <div className='flex h-full md:justify-start justify-center'>
+                <img src={service.logo} className='w-4/6 md:w-4/6 lg:w-4/6' alt="" />
+            </div>
+            <div className='flex flex-col lg:text-left text-center space-y-4'>
+                <div className='md:h-[100px]'>
                     <h3 className='text-xl font-bold mb-2'>{service.name}</h3>
                     <p className='text'>{service.description}</p>
                 </div>
-                <p className='text-blue-400 flex items-center'>Learn More &nbsp; <span><FaArrowRight /></span></p>
+                <div className=' justify-center flex items-center md:justify-start'>
+                    <p className='text-blue-400 me-2'>Learn More</p>
+                    <span className='text-blue-400 '><FaArrowRight /></span>
+                </div>
+
             </div>
         </div>
     );
