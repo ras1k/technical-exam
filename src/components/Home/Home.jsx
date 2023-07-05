50a9c84c13d12a268c23e272f7d65555f86a46f4
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-05 20:21:27 +0600

    fixed responsivenes & added vercel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import Service from '../Service/Service';
 function Home() {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:5000/services')
+    fetch('https://technical-exam-server.vercel.app/services')
       .then(res => res.json())
       .then(data => setServices(data))
   }, [])
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from 'react-icons/fa';
 const Service = ({ service }) => {
     return (
         <div className=''>
-            <img src={service.logo} className='w-4/6 h-full' alt="" />
+            <img src={service.logo} className='w-full lg:w-4/6 h-full' alt="" />
             <div className='flex flex-col space-y-4'>
                 <div className='md:h-[120px]'>
                     <h3 className='text-xl font-bold mb-2'>{service.name}</h3>
